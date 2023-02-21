0c9265ee7744a0ecd8f2903881398e51b1360913
“Saloh <karimov1994@gmail.com>
2023-02-21 11:34:58 +0800

    Initial commit

---
+++ mouse.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+# Get the current position of the mouse cursor
+x, y = pyautogui.position()
+
+# Print the current mouse cursor position
+print(f"Current mouse position: x={x}, y={y}")
